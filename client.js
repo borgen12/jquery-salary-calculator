74f21aacdae45a4008e49a406efd8e3d58213604
Chris Borgen <borgen_12@hotmail.com>
2019-03-10 17:01:59 -0500

    fixing sum month

--- client.js
+++ client.js
@@ -2,6 +2,7 @@ console.log('gret');
 
 $(document).ready(readyNow);
 let employeeList = [];
+let monthSalaries = [];
 
 function readyNow() {
     $('.submitButton').on('click', addEmployee);
@@ -13,9 +14,16 @@ function addEmployee () {
         lastName: $('#lNameInput').val(),
         id: $('#idInput').val(),
         title: $('#titleInput').val(),
-        annualSalary: $('#salaryInput').val()
+        annualSalary: $('#salaryInput').val(),
     }
+    $('#fNameInput, #lNameInput, #idInput, #titleInput, #salaryInput').val('');
+    //clear input fields
     employeeList.push(newEmployee);
+    let monthSalary = (newEmployee.annualSalary)/12;
+    monthSalaries.push(monthSalary);
+    console.log('monthSalaries', monthSalaries);
+    
+
     console.log('newEmployee:', employeeList);
     logEmployee();
 } 
@@ -29,5 +37,28 @@ function logEmployee() {
         $('.emTable').append(`<tr><td>${employee.firstName}</td><td>${employee.lastName}
         </td><td>${employee.id}</td><td>${employee.title}</td><td>${employee.annualSalary}</td></tr>`);
     }
+    monthLog();
+}
 
+function monthLog() {
+    console.log('in monthLog');
+    let clearMonth = $('emTable');
+    clearMonth.empty();
+    let sum = monthSalaries.reduce(function (a, b) { return a + b; }, 0);
+    for( let employee of employeeList) {
+        $('.emFoot').append('<h2>' + sum + '</h2>');
+    }
+}
+
+function sumMonth() {
+    let sum = monthSalaries.reduce(function (a, b) { return a + b; }, 0);
+    return sum;
+
+    /* let sum = 0;
+    for (let i=0; i<monthSalaries.length; i++){
+        console.log(monthSalaries[i]);
+
+        return sum += monthSalaries[i]; */
+console.log(sumMonth);
+ 
 }
